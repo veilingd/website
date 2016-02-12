98c5e129d68c2c70efce0ff6c2d492c726fac56f
veilingd <veilingd@binkmail.com>
2016-02-12 22:37:10 +0000

    throws angular out

--- js/script.js
+++ js/script.js
@@ -3,53 +3,14 @@
 /* main runs upon document ready */
 var main = function() 
 {
-	// activates jquery ui tooltips. The tag's title attribute is displayed on hover.
-	//$(document).tooltip();
-	
-	/* admin panel provides some extra buttons, starts hidden */
-	$('#adminPanel').hide();
-	var buttonOpenedText = "<<< close";
-	var buttonClosedText = "admin >>>";
-	$('#adminPanelToggle').text(buttonClosedText);
-
-	/* button handlers start here */
-	
-	/* toggles admin panel */
-	$('#adminPanelToggle').click(function()
-	{
-		if ($('#adminPanelToggle').text() === buttonClosedText)
-		{
-			$('#adminPanelToggle').text(buttonOpenedText);
-			$('#adminPanel').fadeIn(400);
-		}
-		else
-		{
-			$('#adminPanelToggle').text(buttonClosedText);
-			$('#adminPanel').fadeOut(400);
-		}
-	});
-	
-	/* button handlers stop here */
-
 	//activate all players
 	$( '.isAudioPlayer' ).audioPlayer();
-	
-	//sets first state for tabbed player navigation
-	$('#songTabsButtonList').children().first().addClass('active');
-	$('#songTabsContentList').children().first().addClass('active');
-	$('#songTabsContentList').children().first().addClass('in');
-	//sets id for each tab's content panel
-	$('#songTabsContentList').children().each(function(index) {
-        $(this).attr('id', index);
-    });
     
     // 2 click 'like'
     //defines the default order of the buttons
 	$.fn.socialSharePrivacy.settings.order = ['facebook', 'gplus', 'twitter', 'tumblr', 'reddit'];
 	$.fn.socialSharePrivacy.settings.path_prefix = '../';
  //	$('#socialshareprivacy').socialSharePrivacy({uri: 'http://veilingd.herokuapp.com/'});
-    
-    
     
     $('#share').socialSharePrivacy({
 		perma_option: false,
